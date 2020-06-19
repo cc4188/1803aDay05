d84c61971ec47c12a456e7da185b85361ea87180
cc <wangcc@qq.com>
2020-06-19 08:39:43 +0800

    第一次提交

---
+++ src/main/java/com/wangcc/test/Demo.java
@@ -0,0 +1,11 @@
+package com.wangcc.test;
+
+/**
+ * @author 该账户已被注销
+ * @2020年6月19日
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello,Word!!!");
+	}
+}
